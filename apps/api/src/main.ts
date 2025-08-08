f1c051f2d6500718ffe44e3554700a8d3367b92c
Mateusz Słotwiński <mateuszslotwinski16@gmail.com>
2025-08-08 14:25:31 +0200

    working now

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -6,7 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule)
 
   app.enableCors({
-    origin: 'http://localhost:8080',
+    origin: ['http://localhost:8080', 'http://localhost:3000'],
     credentials: true,
   })
 
--- apps/api/src/providers/AI.ts
+++ apps/api/src/providers/AI.ts
@@ -4,7 +4,7 @@ export const AiCoreProvider = {
   provide: 'AI_CORE_CLIENT',
   useFactory: () => {
     return axios.create({
-      baseURL: /* process.env.AI_CORE_URL  || */ 'http://localhost:8000',
+      baseURL: process.env.AI_CORE_URL || 'http://localhost:8000',
     })
   },
 }
