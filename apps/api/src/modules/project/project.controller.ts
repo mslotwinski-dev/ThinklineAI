3496d2a6543090345c7d7caba856d193785e4c50
Mateusz Słotwiński <mateuszslotwinski16@gmail.com>
2025-08-20 11:40:36 +0200

    api prepared

--- apps/api/src/modules/project/project.controller.ts
+++ apps/api/src/modules/project/project.controller.ts
@@ -30,4 +30,114 @@ export class ProjectController {
   notes(@Body() body: Project) {
     return this.projectService.notes(body)
   }
+
+  @Post('basicfunctions')
+  basicfunctions(@Body() body: Project) {
+    return this.projectService.basicfunctions(body)
+  }
+
+  @Post('extensions')
+  extensions(@Body() body: Project) {
+    return this.projectService.extensions(body)
+  }
+
+  @Post('integrations')
+  integrations(@Body() body: Project) {
+    return this.projectService.integrations(body)
+  }
+
+  @Post('roadmap')
+  roadmap(@Body() body: Project) {
+    return this.projectService.roadmap(body)
+  }
+
+  @Post('interface')
+  interface(@Body() body: Project) {
+    return this.projectService.interface(body)
+  }
+
+  @Post('stack')
+  stack(@Body() body: Project) {
+    return this.projectService.stack(body)
+  }
+
+  @Post('tools')
+  tools(@Body() body: Project) {
+    return this.projectService.tools(body)
+  }
+
+  @Post('architecture')
+  architecture(@Body() body: Project) {
+    return this.projectService.architecture(body)
+  }
+
+  @Post('deployment')
+  deployment(@Body() body: Project) {
+    return this.projectService.deployment(body)
+  }
+
+  @Post('usecases')
+  usecases(@Body() body: Project) {
+    return this.projectService.usecases(body)
+  }
+
+  @Post('patterns')
+  patterns(@Body() body: Project) {
+    return this.projectService.patterns(body)
+  }
+
+  @Post('testing')
+  testing(@Body() body: Project) {
+    return this.projectService.testing(body)
+  }
+
+  @Post('scalability')
+  scalability(@Body() body: Project) {
+    return this.projectService.scalability(body)
+  }
+
+  @Post('monetization')
+  monetization(@Body() body: Project) {
+    return this.projectService.monetization(body)
+  }
+
+  @Post('market')
+  market(@Body() body: Project) {
+    return this.projectService.market(body)
+  }
+
+  @Post('social_impact')
+  socialImpact(@Body() body: Project) {
+    return this.projectService.social_impact(body)
+  }
+
+  @Post('threats')
+  threats(@Body() body: Project) {
+    return this.projectService.threats(body)
+  }
+
+  @Post('monitoring')
+  monitoring(@Body() body: Project) {
+    return this.projectService.monitoring(body)
+  }
+
+  @Post('data_security')
+  dataSecurity(@Body() body: Project) {
+    return this.projectService.data_security(body)
+  }
+
+  @Post('access')
+  access(@Body() body: Project) {
+    return this.projectService.access(body)
+  }
+
+  @Post('acquired_skills')
+  acquiredSkills(@Body() body: Project) {
+    return this.projectService.acquired_skills(body)
+  }
+
+  @Post('further_development')
+  furtherDevelopment(@Body() body: Project) {
+    return this.projectService.further_development(body)
+  }
 }
--- apps/api/src/modules/project/project.service.ts
+++ apps/api/src/modules/project/project.service.ts
@@ -1,6 +1,6 @@
 import { Injectable, Inject } from '@nestjs/common'
 import type { AxiosInstance } from 'axios'
-import { Project } from './project.types'
+import { Project, UseCase } from './project.types'
 
 @Injectable()
 export class ProjectService {
@@ -50,6 +50,163 @@ export class ProjectService {
     return {
       ...project,
       notes: await this.template<string>('notes', project),
+    }
+  }
+
+  async basicfunctions(project: Project): Promise<Project> {
+    return {
+      ...project,
+      basicfunctions: await this.template<string>('basicfunctions', project),
+    }
+  }
+
+  async extensions(project: Project): Promise<Project> {
+    return {
+      ...project,
+      extensions: await this.template<string>('extensions', project),
+    }
+  }
+
+  async integrations(project: Project): Promise<Project> {
+    return {
+      ...project,
+      integrations: await this.template<string>('integrations', project),
+    }
+  }
+
+  async roadmap(project: Project): Promise<Project> {
+    return {
+      ...project,
+      roadmap: await this.template<string>('roadmap', project),
+    }
+  }
+
+  async interface(project: Project): Promise<Project> {
+    return {
+      ...project,
+      interface: await this.template<string>('interface', project),
+    }
+  }
+
+  async stack(project: Project): Promise<Project> {
+    return {
+      ...project,
+      stack: await this.template<string>('stack', project),
+    }
+  }
+
+  async tools(project: Project): Promise<Project> {
+    return {
+      ...project,
+      tools: await this.template<string>('tools', project),
+    }
+  }
+
+  async architecture(project: Project): Promise<Project> {
+    return {
+      ...project,
+      architecture: await this.template<string>('architecture', project),
+    }
+  }
+
+  async deployment(project: Project): Promise<Project> {
+    return {
+      ...project,
+      deployment: await this.template<string>('deployment', project),
+    }
+  }
+
+  async usecases(project: Project): Promise<Project> {
+    return {
+      ...project,
+      usecases: await this.template<UseCase[]>('usecases', project),
+    }
+  }
+
+  async patterns(project: Project): Promise<Project> {
+    return {
+      ...project,
+      patterns: await this.template<string>('patterns', project),
+    }
+  }
+
+  async testing(project: Project): Promise<Project> {
+    return {
+      ...project,
+      testing: await this.template<string>('testing', project),
+    }
+  }
+
+  async scalability(project: Project): Promise<Project> {
+    return {
+      ...project,
+      scalability: await this.template<string>('scalability', project),
+    }
+  }
+
+  async monetization(project: Project): Promise<Project> {
+    return {
+      ...project,
+      monetization: await this.template<string>('monetization', project),
+    }
+  }
+
+  async market(project: Project): Promise<Project> {
+    return {
+      ...project,
+      market: await this.template<string>('market', project),
+    }
+  }
+
+  async social_impact(project: Project): Promise<Project> {
+    return {
+      ...project,
+      social_impact: await this.template<string>('social_impact', project),
+    }
+  }
+
+  async threats(project: Project): Promise<Project> {
+    return {
+      ...project,
+      threats: await this.template<string>('threats', project),
+    }
+  }
+
+  async monitoring(project: Project): Promise<Project> {
+    return {
+      ...project,
+      monitoring: await this.template<string>('monitoring', project),
+    }
+  }
+
+  async data_security(project: Project): Promise<Project> {
+    return {
+      ...project,
+      data_security: await this.template<string>('data_security', project),
+    }
+  }
+
+  async access(project: Project): Promise<Project> {
+    return {
+      ...project,
+      access: await this.template<string>('access', project),
+    }
+  }
+
+  async acquired_skills(project: Project): Promise<Project> {
+    return {
+      ...project,
+      acquired_skills: await this.template<string>('acquired_skills', project),
+    }
+  }
+
+  async further_development(project: Project): Promise<Project> {
+    return {
+      ...project,
+      further_development: await this.template<string>(
+        'further_development',
+        project
+      ),
     }
   }
 }
